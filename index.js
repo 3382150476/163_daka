563f6537812ea9ed5b0255d0b93ade92fdef6627
3382150476 <102964110+3382150476@users.noreply.github.com>
2025-07-16 09:46:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // 用户名和密码
-const phone = '';
-const password = '';
+const phone = '18687827880';
+const password = 'Qq3382150476..';
 
 const rp = require('request-promise');
 const tough = require('tough-cookie');
